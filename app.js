5db0a4c313c2fc4f90ba1de5020f1ce4d9a413a4
utkarsh-minoan <utkarsh@minoanexperience.com>
2024-11-14 21:04:53 +0530

    ap configure

--- app.js
+++ app.js
@@ -25,10 +25,6 @@
 // no matter where we actually lift from.
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const prisma = new PrismaClient();
 require('dotenv').config();
 
 
